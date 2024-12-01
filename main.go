27964b14267ff31f69cc12450478ed9c33b93953
maksimprokopec <maksimka.prokopets@mail.ru>
2024-12-01 20:23:34 +0700

    а

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/Kewere-CTRL/BuilderCompany/internal/db"
 	"github.com/Kewere-CTRL/BuilderCompany/internal/router"
 	"log"
-	"net"
 
 	"github.com/spf13/viper"
 )
@@ -23,38 +22,13 @@ func initConfig() {
 	viper.AutomaticEnv()
 }
 
-func getLocalIP() string {
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		log.Fatal("Ошибка получения интерфейсов: ", err)
-	}
-
-	for _, i := range interfaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			log.Fatal("Ошибка получения адресов интерфейса: ", err)
-		}
-
-		for _, addr := range addrs {
-			// Ищем IPv4 адрес
-			if ipNet, ok := addr.(*net.IPNet); ok && ipNet.IP.To4() != nil {
-				return ipNet.IP.String()
-			}
-		}
-	}
-	return "IP не найден"
-}
-
 func main() {
+
 	initConfig()
 	db.InitDB()
 
 	serverPort := viper.GetInt("server.port")
 
-	// Получаем локальный IP
-	localIP := getLocalIP()
-	fmt.Printf("Сервер запущен на адресе: http://%s:%d\n", localIP, serverPort)
-
 	server := router.SetupRouter()
-	server.Run(fmt.Sprintf("%s:%d", localIP, serverPort))
+	server.Run(fmt.Sprintf("0.0.0.0:%d", serverPort))
 }
